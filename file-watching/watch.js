7254ee0d14158fee482737fc7515b87cd6d1a5d2
Mateusz Pilarczyk <bhakta666@gmail.com>
2021-03-16 16:14:52 +0100

    correction

--- file-watching/watch.js
+++ file-watching/watch.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const file = "./file.txt";
 const moment = require('moment');
+fs.watchFile(file, (current, previous)=>{
+
+
 const time = moment().format("MMMM Do YYYY, h:mm:ss a");
 
 
 
   return console.log(`${file} updated ${time}`);
+});
